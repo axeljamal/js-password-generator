35e57c2e38ffe17b28b7f375beaca10e9aa0a4cf
Axel Jamal <axeltaj@gmail.com>
2023-01-17 19:47:43 +0000

    needed to fix this version

--- script.js
+++ script.js
@@ -89,86 +89,82 @@ var upperCasedCharacters = [
 ];
 
 // Variable Declaration
-let passwordText = document.querySelector("#password");
-let userInput = [];
-let passwordCharacters = [];
-let password = "";
+var passwordText = document.querySelector("#password");
+var userInput = [];
+var password = "";
 
 // Function to prompt user for password options
 function getPasswordOptions() {
   password = "";
-  let passwordCharOption = prompt(
+  var passwordCharOption = prompt(
     "Please enter the number between 10 - 64 for the length of the password",
     ""
   );
 
   passwordLength = parseInt(passwordCharOption, 0);
 
-  // Loop if answer is outside the parameters
-  while (asswordLength <= 10 || passwordLength >= 64) {
-    alert("Password length must be between 10-64 characters Try again");
-    let passwordLength = prompt(
-      "Please enter the number between 10 - 64 for the length of the password"
-    );
-  }
-
-  // Confirm the length of characters
-  alert(`The length of your password will be ${passwordLength} characters`);
-  userInput.push({ PasswordLength: passwordLength });
-
-  // Determine parameters of password
-  let addSpecialChar = confirm(
-    "Click OK to confirm if you would like to add special characters"
-  );
-  let addNumericChar = confirm(
-    "Click OK to confirm if you would like to add numeric characters"
-  );
-  let addUpperCased = confirm(
-    "Click OK to confirm if you would like to add lowercase characters"
-  );
-  let addLowerCased = confirm(
-    "Click OK to confirm if you would like to add uppercase characters"
-  );
-
-  // Loop if user input is outside the parameters
-  while (
-    addUpperCased === false &&
-    addLowerCased === false &&
-    addSpecialChar === false &&
-    addNumericChar === false
-  ) {
-    alert("You must choose at least one parameter");
-    let addSpecialChar = confirm("Click OK to add special characters");
-    let addNumericChar = confirm("Click OK to add numeric characters");
-    let addUpperCased = confirm("Click OK to add lowercase characters");
-    let addLowerCased = confirm("Click OK to add uppercase characters");
-  }
-
-  // Assign an action to the password parameters
-
-  if (lowerCasedChar) {
-    passwordCharacters.push(lowerCasedCharacters);
-  }
-  if (upperCasedChar) {
-    passwordCharacters.push(upperCasedCharacters);
-  }
-  if (specialChar) {
-    passwordCharacters.push(specialCharacters);
-  }
-  if (numericChar) {
-    passwordCharacters.push(numericCharacters);
-  }
-  if (passwordCharacters.length === 0) {
-    alert(
-      "you must select at least one range of characters to generate a password"
-    );
-    getPasswordOptions();
+  if (passwordLength >= 10 && passwordLength <= 64) {
+    alert(`The length of your password will be ${passwordLength} characters`);
+    userInput.push({ PasswordLength: passwordLength });
   } else {
-    generatePassword();
-    passwordCharacters = [];
+    alert("Password length must be between 10-64 characters Try again");
+  }
+  if (passwordLength >= 10 && passwordLength <= 64) {
+    // Determine parameters of password
+    var addSpecialChar = confirm(
+      "Click OK to confirm if you would like to add special characters"
+    );
+    var addNumericChar = confirm(
+      "Click OK to confirm if you would like to add numeric characters"
+    );
+    var addUpperCased = confirm(
+      "Click OK to confirm if you would like to add lowercase characters"
+    );
+    var addLowerCased = confirm(
+      "Click OK to confirm if you would like to add uppercase characters"
+    );
+
+    // Loop if user input is outside the parameters
+    while (
+      addUpperCased === false &&
+      addLowerCased === false &&
+      addSpecialChar === false &&
+      addNumericChar === false
+    ) {
+      alert("You must choose at least one parameter");
+      var addSpecialChar = confirm("Click OK to add special characters");
+      var addNumericChar = confirm("Click OK to add numeric characters");
+      var addUpperCased = confirm("Click OK to add lowercase characters");
+      var addLowerCased = confirm("Click OK to add uppercase characters");
+    }
+
+    // Assign an action to the password parameters
+
+    let passwordCharacters = [];
+
+    if (addLowerCased) {
+      passwordCharacters.push(lowerCasedCharacters);
+    }
+    if (addUpperCased) {
+      passwordCharacters.push(upperCasedCharacters);
+    }
+    if (addSpecialChar) {
+      passwordCharacters.push(specialCharacters);
+    }
+    if (addNumericChar) {
+      passwordCharacters.push(numericCharacters);
+    }
+    if (passwordCharacters.length === 0) {
+      alert(
+        "you must select at least one range of characters to generate a password"
+      );
+      getPasswordOptions();
+    } else {
+      generatePassword();
+      passwordCharacters = [];
+    }
   }
 }
-
 // Function for getting a random element from an array
 function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -176,14 +172,14 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-  for (let i = 0; i < this.passwordLength; i++) {
+  for (var i = 0; i < this.passwordLength; i++) {
     passwordCharacters = passwordCharacters.flat();
     password += getRandom(passwordCharacters);
   }
 }
 
 // Get references to the #generate element
-const generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
